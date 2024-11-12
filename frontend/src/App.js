d9c9d8201f408ba3b3c768c1956c31bab165386a
Stanislaw Cruz <stanycruz@outlook.com>
2024-11-11 23:31:04 -0300

    feat: add tickets to redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import NewTicket from './pages/NewTicket'
+import Tickets from './pages/Tickets'
+import Ticket from './pages/Ticket'
 
 function App() {
   return (
@@ -18,9 +20,30 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/new-ticket" element={<PrivateRoute />}>
-              <Route path="/new-ticket" element={<NewTicket />} />
-            </Route>
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tickets"
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/ticket/:ticketId"
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
